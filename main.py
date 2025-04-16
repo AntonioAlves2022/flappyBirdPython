0d085e15c481d9bf11395a1864bb87235f55e097
Antonio Alves <antonio.alves@duque.g12.br>
2025-04-16 14:26:04 -0300

    Parte 01 da aula

---
+++ main.py
@@ -0,0 +1,44 @@
+import pygame
+import sys
+
+pygame.init()
+SCREEN_WIDTH = 288
+SCREEN_HEIGHT = 512
+FPS = 30
+
+# Desenhar a tela
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Flappy Bird Clone")
+clock = pygame.time.Clock()
+
+# Carregar os sprites
+bg = pygame.image.load("assets/background-day.png").convert()
+ground = pygame.image.load("assets/base.png").convert()
+pipe  = pygame.image.load("assets/pipe-green.png").convert_alpha()
+
+# Sprites dos passaros
+bird_down = pygame.image.load("assets/yellowbird-downflap.png").convert_alpha()
+bird_mid = pygame.image.load("assets/yellowbird-midflap.png").convert_alpha()
+bird_up = pygame.image.load("assets/yellowbird-upflap.png").convert_alpha()
+bird_frames = [bird_down, bird_mid, bird_up]
+
+# Variaveis do Pássaro
+bird_index = 0
+bird_pos_x = 50
+bird_pos_y = 256
+
+# Game loop
+while True:
+    for evt in pygame.event.get():
+        if evt.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        # Atualizar os frames da animação do passaro
+        bird_index = (bird_index +1) % len(bird_frames)
+
+        # Desenhar os elementos
+        screen.blit(bg,(0,0))
+        screen.blit(bird_frames[bird_index],(bird_pos_x, bird_pos_y))
+        screen.blit(ground, (0,SCREEN_HEIGHT- ground.get_height()))
+        pygame.display.update()
+        clock.tick(FPS)
