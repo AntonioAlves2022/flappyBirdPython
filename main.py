694e312a9ead0c6ec8536f33b7c737e20624b614
Antonio Alves <antonio.alves@duque.g12.br>
2025-04-16 15:29:48 -0300

    Parte 03 - Adicionando os canos no game

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import random
 
 pygame.init()
 SCREEN_WIDTH = 288
@@ -15,6 +16,21 @@ bg = pygame.image.load("assets/background-day.png").convert()
 ground = pygame.image.load("assets/base.png").convert()
 pipe  = pygame.image.load("assets/pipe-green.png").convert_alpha()
 
+# Configuração dos canos
+PIPE_DISTANCE = 100
+PIPE_SPEED = 3
+PIPE_HEIGHT = ground.get_height()
+
+def create_pipes():
+    base_height = random.randint(150, 300)
+    y_bottom_pipe = base_height
+    y_top_pipe = base_height - PIPE_DISTANCE - pipe.get_height()
+    return {'x':SCREEN_WIDTH,
+            'top':y_top_pipe,
+            'bottom':y_bottom_pipe
+            }
+
+
 # Sprites dos passaros
 bird_down = pygame.image.load("assets/yellowbird-downflap.png").convert_alpha()
 bird_mid = pygame.image.load("assets/yellowbird-midflap.png").convert_alpha()
@@ -28,6 +44,8 @@ bird_pos_y = 256
 speed = 0 # velocidade do passaro
 gravity = 0.5 # gravidade
 jump = 8 # Força do pulo do passaro
+# Lista de canos
+pipes = [create_pipes()]
 
 # Game loop
 while True:
@@ -45,8 +63,22 @@ while True:
         # Atualizar os frames da animação do passaro
         bird_index = (bird_index +1) % len(bird_frames)
 
+        for p in pipes:
+            p['x'] -= PIPE_SPEED
+        # Adicionar novo cano quando necessário
+        if pipes[-1]['x'] < SCREEN_WIDTH - 150:
+            pipes.append(create_pipes())
+        # Remover canos que não sao mais visiveis
+        if pipes[0]['x'] < -pipe.get_width():
+            pipes.pop(0)
+
         # Desenhar os elementos
         screen.blit(bg,(0,0))
+        # Desenhar os canos
+        for p in pipes:
+            screen.blit(pipe, (p['x'], p['top']))
+            flipped_pipe = pygame.transform.flip(pipe, False, True)
+            screen.blit(flipped_pipe, (p['x'], p['bottom']))
         screen.blit(bird_frames[bird_index],(bird_pos_x, bird_pos_y))
         screen.blit(ground, (0,SCREEN_HEIGHT- ground.get_height()))
         pygame.display.update()
