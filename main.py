2247aa43edf82026b042194f77da34fc835e6352
Antonio Alves <antonio.alves@duque.g12.br>
2025-04-16 16:16:16 -0300

    Parte 04: Criando as colisões e o placar do jogo

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame
 import sys
 import random
-
+# Sistema de pontuação
+pygame.font.init()
+font = pygame.font.SysFont("Comic Sans MS", 32)
+score = 0
 pygame.init()
 SCREEN_WIDTH = 288
 SCREEN_HEIGHT = 512
@@ -17,7 +20,7 @@ ground = pygame.image.load("assets/base.png").convert()
 pipe  = pygame.image.load("assets/pipe-green.png").convert_alpha()
 
 # Configuração dos canos
-PIPE_DISTANCE = 100
+PIPE_DISTANCE = 100 # Usar PIPE_GAP
 PIPE_SPEED = 3
 PIPE_HEIGHT = ground.get_height()
 
@@ -49,6 +52,11 @@ pipes = [create_pipes()]
 
 # Game loop
 while True:
+    # Retângulo do pássaro
+    bird_rect = bird_frames[bird_index].get_rect(center=(
+    bird_pos_x + bird_frames[bird_index].get_width() // 2,
+    bird_pos_y + bird_frames[bird_index].get_height() // 2))
+
     for evt in pygame.event.get():
         if evt.type == pygame.QUIT:
             pygame.quit()
@@ -56,22 +64,26 @@ while True:
         if evt.type == pygame.KEYDOWN:
             if evt.key == pygame.K_SPACE:
                 speed  = - jump
-        # Aplicar gravidade
-        speed += gravity
-        bird_pos_y += speed
+    # Aplicar gravidade
+    speed += gravity
+    bird_pos_y += speed
 
-        # Atualizar os frames da animação do passaro
-        bird_index = (bird_index +1) % len(bird_frames)
+    # Atualizar os frames da animação do passaro
+    bird_index = (bird_index +1) % len(bird_frames)
 
-        for p in pipes:
-            p['x'] -= PIPE_SPEED
+    for p in pipes:
+        p['x'] -= PIPE_SPEED
         # Adicionar novo cano quando necessário
         if pipes[-1]['x'] < SCREEN_WIDTH - 150:
             pipes.append(create_pipes())
         # Remover canos que não sao mais visiveis
         if pipes[0]['x'] < -pipe.get_width():
             pipes.pop(0)
-
+    for p in pipes:
+        if p['x'] + pipe.get_width() // 2 < bird_pos_x and not p.get('scored', False):
+            score += 1
+            p['scored'] = True
+            print(f"Pontuação:  {score}")
         # Desenhar os elementos
         screen.blit(bg,(0,0))
         # Desenhar os canos
@@ -79,7 +91,25 @@ while True:
             screen.blit(pipe, (p['x'], p['top']))
             flipped_pipe = pygame.transform.flip(pipe, False, True)
             screen.blit(flipped_pipe, (p['x'], p['bottom']))
+            top_rect = pipe.get_rect(topleft=(p['x'], p['top']))
+            bottom_rect = pipe.get_rect(topleft=(p['x'], p['bottom']))
+
+            # Verifica a colisão com os canos
+            if bird_rect.colliderect(top_rect) or bird_rect.colliderect(bottom_rect):
+                print("VOCÊ COLIDIU COM O CANO")
+                sys.exit()
+
+            # Verifica a colisão com o chão
+            if bird_pos_y + bird_frames[bird_index].get_height() >= SCREEN_HEIGHT - ground.get_height():
+                print("COLIDIU COM O CHÃO!")
+                pygame.quit()
+                sys.exit()
+
         screen.blit(bird_frames[bird_index],(bird_pos_x, bird_pos_y))
         screen.blit(ground, (0,SCREEN_HEIGHT- ground.get_height()))
+        # Mostrando o placar na tela
+        score_surface = font.render(str(score), True, (255, 255, 255))
+        score_rect = score_surface.get_rect(center=(SCREEN_WIDTH // 2, 50))
+        screen.blit(score_surface, score_rect)
         pygame.display.update()
         clock.tick(FPS)
